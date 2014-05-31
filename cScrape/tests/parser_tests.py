b98ee4db359828f281463d216aa5ee50a26a1e8a
Andy Perez <ap469@cornell.edu>
2014-05-31 19:21:58 +0000

    tests for extractURLandTitle() and for extractLocation()

--- cScrape/tests/parser_tests.py
+++ cScrape/tests/parser_tests.py
@@ -77,5 +77,54 @@ class extractPID_tests(unittest.TestCase):
 		self.secondJobEntry.close()
 		self.lastJobEntry.close()
 
+class extractURLandTitle_tests(unittest.TestCase):
+
+	def setUp(self):
+		files = []
+
+		files.append( open( os.path.join(res_dir, "firstEntry.txt"), 'r') )
+		files.append( open( os.path.join(res_dir, "secondEntry.txt"), 'r') )
+		files.append( open( os.path.join(res_dir, "lastEntry.txt"), 'r') )
+		
+		self.URLs = []
+		self.titles = []
+		for i, entryFile in files:
+			URL, title = pageParse.extractURLandTitle(entryFile.read())
+			self.URLs.append(URL)
+			self.titles.append(title)
+			files[i].close()	
+
+	def test_extractURLandTitle_correctURLExtracted(self):
+		correctURLs = ["/mnh/rej/4496353905.html", "/brk/rej/4496348548.html", "/brx/rej/4492280745.html"]
+		for i, URL in enumerate(self.URLs):
+			self.assertEqual(URL, correctURLs[i], "Evaluated URL: {} Actual URL: {}".format(URL, correctURLs[i]))
+		
+	def test_extractURLandTitle_correctTitleExtracted(self):
+		correctTitles = [ "MAKE $$$ Now!! No Exp. Needed! BUSY, Fun Office. ", "FT SECRETARY FOR REAL ESTATE OFFICE", "Experienced LIHTC Property Manager Needed"]
+		for i, title in enumerate(self.titles):
+			self.assertEqual(title, correctTitles[i], "Evaluated Title: {} Actual Title: {}".format(title, correctTitles[i]))
+
+
+class extractLocation_tests(unittest.TestCase):
+
+	def setUp(self):	
+		self.files = []
+
+		self.files.append( open( os.path.join(res_dir, "firstEntry.txt"), 'r') )
+		self.files.append( open( os.path.join(res_dir, "secondEntry.txt"), 'r') )
+		self.files.append( open( os.path.join(res_dir, "lastEntry.txt"), 'r') )
+	
+	def test_extractLocation_badParse(self):
+		locationHTML = 'blahblahrandomstuff<span></span> morerandom'
+		result = pageParse.extractLocation(locationHTML)
+		self.assertEqual(result, '', "extractLocation not empty string on bad parse.  Output given was: {}".format(result)
+
+	def test_extractLocation_correctEntryParsing(self):
+		correctLocations = [ "Manhattan", "Clinton Hill", "Bronx" ]
+		for i, entryFile in enumerate(self.files):
+		output = pageParse.extractLocation(entryFile.read())
+		self.assertEqual(correctLocations[i], output, "Evaluated output: {}  Expected output: {}".format(output, correctLocations[i]))
+
+
 if __name__ == "__main__":
 	unittest.main()
